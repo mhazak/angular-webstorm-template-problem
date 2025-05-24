4b60f5ea2dc792ddc001fde91403d4f738c111ff
hazak.marek <hazak.marek@gmail.com>
2025-05-24 13:33:30 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {CommonModule} from "@angular/common";
+import {ButtonModule} from "primeng/button";
+
+type Create = {
+  mode: 'CREATE'
+}
+
+type Update = {
+  mode: 'UPDATE',
+  id: number;
+}
+
+type Union = Create | Update;
+
+type VM = {
+  detail: Union;
+  commonProp1: string;
+  commonProp2: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'webstorm-template-problem';
+  vm: VM = {
+    detail: {
+      mode: 'CREATE'
+    },
+    commonProp1: 'blabla',
+    commonProp2: 'blabla2'
+  }
+
+  onClick(id: number) {
+
+  }
 }
